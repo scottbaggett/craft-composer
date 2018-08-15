a2ff823195646bc9af0b6befd198f7de3aee8323
Scott Baggett <scott@superformula.com>
2018-08-15 16:05:17 -0700

    first

---
+++ src/translations/en/craft-composer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Craft Composer plugin for Craft CMS 3.x
+ *
+ * Superformula's composer plugin
+ *
+ * @link      superformula.com
+ * @copyright Copyright (c) 2018 Scott Baggett
+ */
+
+/**
+ * Craft Composer en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('craft-composer', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Scott Baggett
+ * @package   CraftComposer
+ * @since     1.0.0
+ */
+return [
+    'Craft Composer plugin loaded' => 'Craft Composer plugin loaded',
+];
